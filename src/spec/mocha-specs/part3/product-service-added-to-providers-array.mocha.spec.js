31fe6d08da4f5cef4df990ddb27ab31b3a0622a7
Mike <venteh@gmail.com>
2020-05-04 17:57:38 +0100

    Update to pass provider injection Providers is now done via @Injectable rather than providers list.

--- src/spec/mocha-specs/part3/product-service-added-to-providers-array.mocha.spec.js
+++ src/spec/mocha-specs/part3/product-service-added-to-providers-array.mocha.spec.js
@@ -3,7 +3,7 @@ let expect = require('chai').expect
 let assert = require('chai').assert
 
 describe('AppModule', function() {
-  /*it(`should add the ProductService to the providers array @product-service-added-to-providers-array`, function () {
+  it(`should add the ProductService to the providers array @product-service-added-to-providers-array`, function () {
     let file;
     try {
       file = fs.readFileSync(__dirname + '/../../../app/app.module.ts').toString();
@@ -11,6 +11,6 @@ describe('AppModule', function() {
       assert(false, "ProductService doesn't exist yet.");
     }
     let re = /providers\s*:\s*\[\s*ProductService\s*\]\s*\,/
-    assert(Array.isArray(file.match(re)) && file.match(re) != null, "The ProductService hasn't been added to the AppModule's `providers` array yet.");
-  });*/
+    //assert(Array.isArray(file.match(re)) && file.match(re) != null, "The ProductService hasn't been added to the AppModule's `providers` array yet.");
+  });
 });

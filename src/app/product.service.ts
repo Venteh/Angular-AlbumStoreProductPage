2064c285cf8efa09f221986b00f79466cefdf438
Mike Flynn <venteh@gmail.com>
2020-05-05 11:36:02 +0100

    Complete Part 6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpResponse } from "@angular/common/http";
 import { map } from "rxjs/operators";
 import { Observable } from "rxjs/Observable";
 import { Album } from "./album";
+import { Product } from './product';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,7 @@ import { Album } from "./album";
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: HttpClient) { }
 
@@ -18,4 +20,10 @@ export class ProductService {
       map(response => response.album as Album)
     );
   }
+
+  getProducts() : Observable<Product[]> {
+    return this._http.get<Product[]>(this._productsUrl);
+  }
+
+
 }
